6dfab02054260707fa5915842305691406388e24
Prakharpatel-24 <prakharpatel2003@gmail.com>
2023-03-25 17:39:52 +0530

    Added Traffic, Transport, Hospital's portal

---
+++ server/modal/Transportuser.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const transportuserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('transportuser', transportuserSchema);
+
+// 15:37
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -89,6 +89,54 @@ function Navbar() {
               </NavLink>
             </li>
             <li>
+              <NavLink
+                to="/hospital/hosplogin"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Hospital Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/hospital/hospregister"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Hospital Register
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/traffic/trafficlogin"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Traffic Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/traffic/trafficregister"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Traffic register
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/transport/transportlogin"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Transport Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/transport/transportregister"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Transport register
+              </NavLink>
+            </li>
+            <li>
               {/* <NavLink to="/success" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Success</NavLink> */}
             </li>
           </ul>
