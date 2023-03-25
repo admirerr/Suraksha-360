85ede9cbf180e7e13c431faf3a408e0e9f357f83
Shubham <shubhamkashyap0608@gmail.com>
2023-03-25 14:54:55 +0530

    Added sidebar to the main landing page

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
-  const [username, setUsername] = useState("");
+  const [username, setName] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,7 +14,7 @@ function LandingPage() {
       })
       .then((res) => {
         console.log(res);
-        setUsername(res.data.username);
+        setName(res.data.username);
         localStorage.setItem("username", res.data.username);
       })
       .catch((err) => {
@@ -28,16 +28,54 @@ function LandingPage() {
   }
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold">
-        Welcome {username ? username : "User"} to Landing Page
-      </h1>
-      <button
-        className="w-full flex justify-center items-center bg-red-300 py-3 rounded-lg mt-8"
-        onClick={handleLogout}
-      >
-        Logout
-      </button>
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="bg-gray-200 h-screen w-64 flex flex-col">
+        <div className="py-4 px-6">
+          <h2 className="text-lg font-semibold mb-4">Menu</h2>
+          <ul>
+            <li className="mb-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600 font-semibold">
+                Option 1
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600 font-semibold">
+                Option 2
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600 font-semibold">
+                Option 3
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600 font-semibold">
+                Option 4
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600 font-semibold">
+                Option 5
+              </a>
+            </li>
+          </ul>
+          <button className="bg-red-500 text-white font-semibold py-2 px-4 mt-4 rounded hover:bg-red-600" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      </div>
+
+      {/* Main content */}
+      <div className="w-full bg-gray-100">
+    
+        {/* Main content area */}
+        <div className="w-full h-screen flex justify-center items-center">
+          <h1 className="text-4xl font-bold text-gray-800">
+            Welcome {username ? username : "User"} to Landing Page
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
